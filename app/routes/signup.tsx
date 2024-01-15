3734379bdc63a413e5df9a1e3c5af9ca59fa0a1e
Brandon Lyons <brandonl.4231@gmail.com>
2024-01-15 03:48:17 -0800

    Update signup.tsx

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -138,7 +138,7 @@ export default function SignUp() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Have an account?{" "}
             <a
-              href="/login"
+              href="/signin"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Login
