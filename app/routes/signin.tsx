19d39d3f4f5caccd7da94773aaecf3392e424035
Brandon Lyons <lyonsbp@umich.edu>
2024-01-16 01:28:33 -0800

    Style

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -26,7 +26,7 @@ export async function action({ request, context }: ActionFunctionArgs) {
 export default function SignIn() {
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center py-8 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <Image
             className="mx-auto"
